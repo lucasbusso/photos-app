d913654d1d0759e738556c92cd6a9b7216a90243
Lucas Busso <lucasxbusso@gmail.com>
2023-08-06 18:13:50 -0300

    home and gallery routes. capacitor camera

---
+++ src/app/pages/gallery/gallery.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from 'src/app/services/photos.service';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+})
+export class GalleryComponent implements OnInit {
+  photos: string[] = [];
+
+  constructor(private photoService: PhotosService) {
+    this.photos = this.photoService.photos;
+  }
+
+  ngOnInit(): void {}
+
+  async takePhoto() {
+    await this.photoService.addNewPhoto();
+  }
+}
